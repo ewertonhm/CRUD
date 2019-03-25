2557cbc2e9a3dda64728841e753c22ee4281a95d
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-03-25 17:50:45 -0300

    Update cadcli.php

--- cadcli.php
+++ cadcli.php
@@ -3,14 +3,13 @@
 require_once "vendor/autoload.php";
 
 $compiler = new \App\Controllers\PageCompiler('cadcli');
+$c = new \App\Models\Cliente();
 
-if(!isset($_POST['btn-cadastrar'])){
-    $c = new \App\Models\Cliente();
+if(isset($_POST['btn-cadastrar'])){
     $c->setNomeCliente($_POST['nome']);
     $c->setTelefoneCliente($_POST['fone']);
     $c->setDataNascimentoCliente($_POST['bday']);
     $c->setCpfCliente($_POST['cpf']);
     $c->criarCliente();
     $id = $c->get_lastInsertID();
-    echo "Cadastro criado com o id: $id";
 }
